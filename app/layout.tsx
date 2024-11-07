4cf075d0e6df057b816ace4501755a34daa5c045
Seo <seo@dayoff.co.kr>
2024-11-07 16:58:20 +0900

    Add shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import React from "react";
 
+import Navbar from "@/components/navigation/navbar";
 import ThemeProvider from "@/context/theme";
 import "./globals.css";
 
@@ -41,6 +42,7 @@ export default function RootLayout({
           enableSystem={true}
           disableTransitionOnChange={true}
         >
+          <Navbar />
           {children}
         </ThemeProvider>
       </body>
