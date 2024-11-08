cc9b629f609846b8445ad1d7a8a6c7b15a7416ab
Seo <seo@dayoff.co.kr>
2024-11-08 14:49:21 +0900

    Mobile nav and nav links

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import MobileNavigation from "./MobileNavigation";
 import Theme from "./Theme";
 
 const Navbar = () => {
@@ -30,6 +31,7 @@ const Navbar = () => {
 
       <div className="flex-between gap-5">
         <Theme />
+        <MobileNavigation />
       </div>
     </nav>
   );
